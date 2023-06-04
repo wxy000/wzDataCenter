05197adfb2d8566f74e085d1cbc6af30ee4a27b5
wxy000 <396310583@qq.com>
2023-06-04 10:37:06 +0800

    增加按‘客户’分析数据

--- app/zentao/zentao_models/dbmodel.go
+++ app/zentao/zentao_models/dbmodel.go
@@ -6,3 +6,10 @@ type Leixing struct {
 	Esti      float64 `json:"esti"`      //预计工时
 	Cons      float64 `json:"cons"`      //实际工时
 }
+
+// Customer 客户表
+type Customer struct {
+	Customername string  `json:"customername"` //客户
+	Esti         float64 `json:"esti"`         //预计工时
+	Cons         float64 `json:"cons"`         //实际工时
+}
--- app/zentao/zentao_models/dboperate.go
+++ app/zentao/zentao_models/dboperate.go
@@ -4,8 +4,8 @@ import (
 	"wzDataCenter/app/zentao/zentao_common"
 )
 
-// GetLeixing 按照用户、起止时间获取‘类型’数据
-func GetLeixing(userId uint, dateStart string, dateEnd string) (bool, *[]Leixing, int64) {
+// GetAnalysisLeixing 按照用户、起止时间获取‘类型’数据
+func GetAnalysisLeixing(userId uint, dateStart string, dateEnd string) (bool, *[]Leixing, int64) {
 	var d1 []Leixing
 	sql := `SELECT cloudname,sum(tt.esti) esti,sum(tt.cons) cons
 			  FROM (SELECT t3.id id,t1.name namec,t1.project proj,t3.account acct,t3.date dat,
@@ -53,6 +53,62 @@ func GetLeixing(userId uint, dateStart string, dateEnd string) (bool, *[]Leixing
 			   and tt.acct = ?
 			 GROUP BY tt.acct,tt.tycc
 			 ORDER BY tt.acct,tt.tycc`
+	res := zentao_common.ZENTAO_DB.Raw(sql, dateStart, dateEnd, userId)
+	r1 := res.Scan(&d1)
+	if r1.Error != nil {
+		return false, nil, 0
+	}
+	return true, &d1, r1.RowsAffected
+}
+
+// GetAnalysisCustomer 按照用户、起止时间获取‘类型’数据
+func GetAnalysisCustomer(userId uint, dateStart string, dateEnd string) (bool, *[]Customer, int64) {
+	var d1 []Customer
+	sql := `SELECT t0.name customername,sum(tt.esti) esti,sum(tt.cons) cons
+          	  FROM (SELECT t3.id id,t1.name namec,t1.project proj,t3.account acct,t3.date dat,
+          	               t1.estimate esti,t3.consumed cons,t1.type tycc
+          	          FROM zt_effort t3
+          	          LEFT JOIN zt_task t1 ON t1.id = t3.objectID
+          	         WHERE t1.deleted <> 2
+          	           AND t3.deleted <> 2
+          	           AND t1.status IN ( 'closed', 'done' )
+          	           AND t1.type = 'T0343'
+          	           AND NOT EXISTS (SELECT 1 FROM zt_task t2 WHERE t2.parent = t1.id AND t2.deleted <> 2 )
+          	           AND EXISTS (SELECT 1 FROM (SELECT id,ROW_NUMBER() over ( PARTITION BY objectID ORDER BY date,id) rk
+          	                                        FROM zt_effort
+          	                                       WHERE deleted <> 2 ) t6
+          	                               WHERE rk = 1
+          	                                 AND t6.id = t3.id)
+          	         UNION ALL
+          	        SELECT t3.id id,t1.name namec,t1.project proj,t3.account acct,t3.date dat,
+          	               0 esti,t3.consumed cons,t1.type tycc
+          	          FROM zt_effort t3
+          	          LEFT JOIN zt_task t1 ON t1.id = t3.objectID
+          	         WHERE t1.deleted <> 2
+          	           AND t3.deleted <> 2
+          	           AND t1.status IN ( 'closed', 'done' )
+          	           AND t1.type = 'T0343'
+          	           AND NOT EXISTS (SELECT 1 FROM zt_task t2 WHERE t2.parent = t1.id AND t2.deleted <> 2 )
+          	           AND EXISTS (SELECT 1 FROM (SELECT id,ROW_NUMBER() over ( PARTITION BY objectID ORDER BY date,id) rk
+          	                                        FROM zt_effort
+          	                                       WHERE deleted <> 2 ) t6
+          	                               WHERE rk > 1
+          	                                 AND t6.id = t3.id)
+          	         UNION ALL
+          	        SELECT t3.id id,t1.name namec,t1.project proj,t3.account acct,t3.date dat,
+          	               t3.consumed esti,t3.consumed cons,t1.type tycc
+          	          FROM zt_effort t3
+          	          LEFT JOIN zt_task t1 ON t1.id = t3.objectID
+          	         WHERE t1.deleted <> 2
+          	           AND t3.deleted <> 2
+          	           AND t1.status IN ( 'closed', 'done' )
+          	           AND t1.type <> 'T0343'
+          	           AND NOT EXISTS (SELECT 1 FROM zt_task t2 WHERE t2.parent = t1.id AND t2.deleted <> 2 )) tt
+          	  LEFT JOIN zt_project t0 ON t0.id = tt.proj
+          	 WHERE tt.dat BETWEEN ? AND ?
+          	   and tt.acct = ?
+          	 GROUP BY tt.acct,tt.proj
+          	 ORDER BY tt.acct,tt.proj`
 	res := zentao_common.ZENTAO_DB.Raw(sql, dateStart, dateEnd, userId)
 	r1 := res.Scan(&d1)
 	if r1.Error != nil {
