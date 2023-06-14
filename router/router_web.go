fd84c925a80835e0e892b0909443ed36b8bb8438
wxy000 <396310583@qq.com>
2023-06-14 18:00:54 +0800

    增加web页面（示例）-调整1

--- router/router_web.go
+++ router/router_web.go
@@ -3,7 +3,6 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"wzDataCenter/middleware"
 )
 
 func RouterWeb(r *gin.Engine) {
@@ -13,8 +12,9 @@ func RouterWeb(r *gin.Engine) {
 	r.Static("/img", "./web/img")
 	r.Static("/css", "./web/css")
 	r.Static("/js", "./web/js")
+	r.Static("/layui", "./web/layui")
 
-	web := r.Group("/web", middleware.JWTAuth())
+	web := r.Group("/web")
 	{
 		web.GET("/hellohtml", func(ctx *gin.Context) {
 			path := "请求路径：" + ctx.FullPath()
