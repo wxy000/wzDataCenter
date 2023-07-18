d996311f0f921622b1e632618ba4feb5275de51a
wxy000 <396310583@qq.com>
2023-07-18 14:11:19 +0800

    修正登录、获取用户信息的问题

--- controllers/authController.go
+++ controllers/authController.go
@@ -12,6 +12,18 @@ func AuthHandler(ctx *gin.Context) {
 	// 用户发送用户名和密码过来
 	username := ctx.PostForm("username")
 	password := ctx.PostForm("password")
+	if username == "" {
+		var login struct {
+			Username string `json:"username"`
+			Password string `json:"password"`
+		}
+		err := ctx.BindJSON(&login)
+		if err != nil {
+			common.FailWithMsg(err.Error(), ctx)
+		}
+		username = login.Username
+		password = login.Password
+	}
 	// 校验用户名和密码是否正确
 	user, err := models.LoginByUsername(username, password)
 	if err != nil {
@@ -35,6 +47,8 @@ func GetUserInfo(ctx *gin.Context) {
 
 // UpdateUserInfo 修改用户信息
 func UpdateUserInfo(ctx *gin.Context) {
+	user_mid, _ := ctx.Get("claims")
+	userid := user_mid.(*utils.CustomClaims).Users.ID
 	user := models.Users{
 		Username: ctx.PostForm("username"),
 		Realname: ctx.PostForm("realname"),
@@ -43,17 +57,23 @@ func UpdateUserInfo(ctx *gin.Context) {
 		Gender:   ctx.PostForm("gender"),
 		Mark:     ctx.PostForm("mark"),
 	}
-	if err := models.UpdateUserInfoByID(user); err != nil {
+	if err := models.UpdateUserInfoByID(userid, user); err != nil {
 		common.FailWithMsg(err.Error(), ctx)
 	} else {
-		common.OkWithMsg("用户信息更新成功", ctx)
+		_, userNew, err := models.GetUserInfoByID(userid)
+		if err != nil {
+			common.FailWithMsg(err.Error(), ctx)
+		}
+		// 生成新的Token
+		tokenNew, _ := utils.NewJWT().GenToken(*userNew)
+		common.OkWithDetailed("用户信息更新成功", gin.H{"token": tokenNew}, ctx)
 	}
 }
 
 // UpdatePassword 修改密码
 func UpdatePassword(ctx *gin.Context) {
-	user, _ := ctx.Get("user")
-	userid := user.(models.Users).ID
+	user, _ := ctx.Get("claims")
+	userid := user.(*utils.CustomClaims).Users.ID
 	oldpw := ctx.PostForm("oldpassword")
 	password := ctx.PostForm("password")
 	if err := models.UpdatePasswordByID(userid, oldpw, password); err != nil {
--- models/dboperate_auth.go
+++ models/dboperate_auth.go
@@ -11,7 +11,7 @@ func GetUserInfoByID(userID uint) (int64, *Users, error) {
 	if userID == 0 {
 		return 0, nil, errors.New("用户编号不得为空")
 	}
-	result := common.DB.Where("userid = ?", userID).Limit(1).Find(&user)
+	result := common.DB.Where("id = ?", userID).Limit(1).Find(&user)
 	if result.Error != nil {
 		return 0, nil, result.Error
 	}
@@ -51,8 +51,8 @@ func LoginByUsername(username string, password string) (*Users, error) {
 }
 
 // UpdateUserInfoByID 根据id更新用户信息
-func UpdateUserInfoByID(user Users) error {
-	result := common.DB.Model(&user).Omit("userid", "password").Where("userid = ?", user.ID).Updates(user)
+func UpdateUserInfoByID(userid uint, user Users) error {
+	result := common.DB.Model(&user).Omit("id", "password").Where("id = ?", userid).Updates(user)
 	if result.RowsAffected < 1 {
 		return result.Error
 	}
@@ -62,14 +62,14 @@ func UpdateUserInfoByID(user Users) error {
 // UpdatePasswordByID 根据id修改用户密码
 func UpdatePasswordByID(userid uint, oldpw string, password string) error {
 	var user Users
-	r1 := common.DB.Where("userid = ? and password = ?", userid, oldpw).First(&user)
+	r1 := common.DB.Where("id = ? and password = ?", userid, oldpw).First(&user)
 	if r1.RowsAffected < 1 {
 		return errors.New("旧密码错误")
 	}
 	if oldpw == password {
 		return errors.New("新密码不得与旧密码相同")
 	}
-	r2 := common.DB.Model(&user).Where("userid = ?", userid).Update("password", password)
+	r2 := common.DB.Model(&user).Where("id = ?", userid).Update("password", password)
 	if r2.RowsAffected < 1 {
 		return errors.New("密码修改失败，请稍后再试")
 	}
